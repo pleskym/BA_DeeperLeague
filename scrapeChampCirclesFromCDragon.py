bfc135d08e79a27995d37aedbc0ee635c01da0a1
pleskym <marcel.plesky68@gmail.com>
2025-03-06 21:40:33 +0100

    Adding newer champions

--- scrapeChampCirclesFromCDragon.py
+++ scrapeChampCirclesFromCDragon.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-champions_static = 'http://ddragon.leagueoflegends.com/cdn/13.14.1/data/en_US/champion.json'
+champions_static = 'https://ddragon.leagueoflegends.com/cdn/15.3.1/data/en_US/champion.json'
 
 champions = requests.get(champions_static).json()['data']
 
@@ -13,52 +13,61 @@ os.makedirs('./champions', exist_ok=True)
 
 for champ_name in champ_names:
   lower_case_champ_name = champ_name.lower()
-  champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/{0}/hud/{0}_circle.png'.format(lower_case_champ_name)
-  champ_path_opt_2 = 'https://raw.communitydragon.org/13.14/game/assets/characters/{0}/hud/{0}_circle_0.png'.format(lower_case_champ_name)
+  champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/{0}/hud/{0}_circle.png'.format(lower_case_champ_name)
+  champ_path_opt_2 = 'https://raw.communitydragon.org/latest/game/assets/characters/{0}/hud/{0}_circle_0.png'.format(lower_case_champ_name)
   if(lower_case_champ_name == 'udyr'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/udyr/hud/udyr_circle_0.udyrvgu.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/udyr/hud/udyr_circle_0.udyrvgu.png'
 
   if(lower_case_champ_name == 'ahri'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/ahri/hud/ahri_circle_0.skins_ahri_asu_prepro.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/ahri/hud/ahri_circle_0.skins_ahri_asu_prepro.png'
 
   if(lower_case_champ_name == 'anivia'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/anivia/hud/cryophoenix_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/anivia/hud/cryophoenix_circle.png'
+
+  if(lower_case_champ_name == 'ambessa'):
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/ambessa/hud/ambessa_circle_0.domina.png'
+    
+  if(lower_case_champ_name == 'aurora'):
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/aurora/hud/aurora_circle_0.aurora.png'
 
   if(lower_case_champ_name == 'belveth'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/belveth/hud/belveth_circle_0.belveth.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/belveth/hud/belveth_circle_0.belveth.png'
 
   if(lower_case_champ_name == 'blitzcrank'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/blitzcrank/hud/steamgolem_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/blitzcrank/hud/steamgolem_circle.png'
 
   if(lower_case_champ_name == 'chogath'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/chogath/hud/greenterror_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/chogath/hud/greenterror_circle.png'
   
   if(lower_case_champ_name == 'hecarim'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/hecarim/hud/hecarim_circle_0.pie_c_13_6.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/hecarim/hud/hecarim_circle_0.pie_c_13_6.png'
 
   if(lower_case_champ_name == 'ksante'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/ksante/hud/ksante_circle_0.ksante.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/ksante/hud/ksante_circle_0.ksante.png'
 
-  if(lower_case_champ_name == 'milio'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/milio/hud/milio_circle_0.milio.png'
-
-  if(lower_case_champ_name == 'naafiri'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/naafiri/hud/naafiri_circle.naafiri.png'
+  if(lower_case_champ_name == 'mel'):
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/mel/hud/mel_circle_0.mel.png'
 
   if(lower_case_champ_name == 'nilah'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/nilah/hud/nilah_circle_0.nilah.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/nilah/hud/nilah_circle_0.nilah.png'
 
   if(lower_case_champ_name == 'orianna'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/orianna/hud/oriana_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/orianna/hud/oriana_circle.png'
 
   if(lower_case_champ_name == 'rammus'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/rammus/hud/armordillo_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/rammus/hud/armordillo_circle.png'
 
   if(lower_case_champ_name == 'shaco'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/shaco/hud/jester_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/shaco/hud/jester_circle.png'
+
+  if(lower_case_champ_name == 'teemo'):
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/teemo/hud/teemo_circle_0.asu_teemo.png'
+  
+  if(lower_case_champ_name == 'viktor'):
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/viktor/hud/viktor_circle_0.viktorvgu.png'
 
   if(lower_case_champ_name == 'zilean'):
-    champ_path_opt_1 = 'https://raw.communitydragon.org/13.14/game/assets/characters/zilean/hud/chronokeeper_circle.png'
+    champ_path_opt_1 = 'https://raw.communitydragon.org/latest/game/assets/characters/zilean/hud/chronokeeper_circle.png'
 
   response = requests.get(champ_path_opt_1)
   if response.status_code != 200:
